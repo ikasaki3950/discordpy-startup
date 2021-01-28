af254e8c177ed5497c7a63a4beb42d1d0942f3f3
ikasaki3950 <72529380+ikasaki3950@users.noreply.github.com>
2021-01-29 08:58:20 +0900

    あああああ

--- discordbot.py
+++ discordbot.py
@@ -33,3 +33,26 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
+
+#coding:UTF-8
+import discord
+from discord.ext import tasks
+from datetime import datetime 
+
+CHANNEL_ID = 804500361812770816 #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '09:00':
+        channel = client.get_channel(804500361812770816)
+        await channel.send('おはよー')  
+
+#ループ処理実行
+loop.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
