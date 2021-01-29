bcaaef795759509af78670a2b8d3aab1ab77baf0
ikasaki3950 <72529380+ikasaki3950@users.noreply.github.com>
2021-01-29 09:02:30 +0900

    あああああ

--- discordbot.py
+++ discordbot.py
@@ -18,6 +18,14 @@ async def おはよー(ctx):
     await ctx.send('おっはー！！')
     
 @bot.command()
+async def おはよう(ctx):
+    await ctx.send('おっはー！！')
+    
+@bot.command()
+async def こんちわー(ctx):
+    await ctx.send('こんちゃ！！')  
+    
+@bot.command()
 async def こんにちは(ctx):
     await ctx.send('こんちゃ！！')
     
@@ -33,26 +41,3 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
-
-#coding:UTF-8
-import discord
-from discord.ext import tasks
-from datetime import datetime 
-
-CHANNEL_ID = 804500361812770816 #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '09:00':
-        channel = client.get_channel(804500361812770816)
-        await channel.send('おはよー')  
-
-#ループ処理実行
-loop.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
